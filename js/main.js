9c2bc7166878c93330aa015a270a09fa8e2b1936
Dhawal Arora <119643277+dhawal-arora@users.noreply.github.com>
2024-06-26 16:36:44 -0400

    Initial Build

---
+++ js/main.js
@@ -0,0 +1,278 @@
+function help(){
+  return "<h2><span style=\"color:#eb926d;\">Help:</span></h2><table>\
+  <tr>\
+    <td>all</td>\
+    <td>Get a comprehensive overview.</td>\
+  </tr>\
+  <tr>\
+    <td>about</td>\
+    <td>Learn more about my background and interests.</td>\
+  </tr>\
+  <tr>\
+    <td>contact</td>\
+    <td>Find out how to reach me.</td>\
+  </tr>\
+  <tr>\
+    <td>links</td>\
+    <td>Explore recommended links.</td>\
+  </tr>\
+  <tr>\
+    <td>skills</td>\
+    <td>Discover my proficiency in various areas.</td>\
+  </tr>\
+  <tr>\
+    <td>experience</td>\
+    <td>View my professional journey.</td>\
+  </tr>\
+  <tr>\
+    <td>projects</td>\
+    <td>See some of my notable projects.</td>\
+  </tr>\
+  <tr>\
+    <td>awards</td>\
+    <td>Check out awards received by me.</td>\
+  </tr>\
+  <tr>\
+    <td>creativity</td>\
+    <td>Explore my creative works.</td>\
+  </tr>\
+  <tr>\
+    <td>extracurricular</td>\
+    <td>Learn about my hobbies and interests.</td>\
+  </tr>\
+  <tr>\
+    <td>rickroll</td>\
+    <td>Get Rickrolled</td>\
+  </tr></table>";
+}
+
+
+function contact() {
+  return "<h2><span style=\"color:#cc6666;\">Contact:</span></h2><table>\
+  <tr>\
+    <td>Email</td>\
+    <td><a href=\"mailto:dhawalchess@gmail.com\">dhawalchess@gmail.com</a></td>\
+  </tr>\
+  </table>";
+}
+
+
+function skills(){
+  return "<h2><span style=\"color:#81a2be;\">Skills:</span></h2><table>\
+  <tr>\
+    <td>Languages:</td>\
+    <td>Python, Java, R, HTML, CSS, JavaScript, C</td>\
+  </tr>\
+  <tr>\
+    <td>Library/Framework: </td>\
+    <td>Flask, FastAPI, Vue.js, Next.js, Nuxt.js, Pandas, Numpy, Matplotlib, BS4, Django, LangChain, Three.js, A-Frame</td>\
+  </tr>\
+  <tr>\
+    <td>Databases:</td>\
+    <td>MySQL, PostgreSQL, Prisma ORM, SQLAlchemy ORM, Chroma DB</td>\
+  </tr>\
+  <tr>\
+    <td>Technologies:</td>\
+    <td> Git, AWS, S3, REST-API, CRM, LLM, API Development</td>\
+  </tr></table>";
+}
+
+function about(){
+  return "<p>Computer science student with a passion for creating 𝗜𝗠𝗣𝗔𝗖𝗧𝗙𝗨𝗟 software that people will actually use.</p>";
+}
+
+function links(){
+  return "<span style=\"color: #b5bd68;\"><h2>Links:</h2></span><ul>\
+  <li><a href=\"https://www.linkedin.com/in/dhawal15/\" target=\"_blank\"><i class=\"fab fa-linkedin\"></i> LinkedIn</a></li>\
+  <li><a href=\"https://github.com/dhawal-arora\" target=\"_blank\"><i class=\"fab fa-github\"></i> Github</a></li>\
+  <li><a href=\"https://www.youtube.com/@dhawalchess\" target=\"_blank\"><i class=\"fab fa-youtube\"></i> Youtube</a></li>\
+  <li><a href=\"https://podcasters.spotify.com/pod/show/thed4podcast\" target=\"_blank\"><i class=\"fab fa-spotify\"></i> Podcast</a></li>\
+  </ul>";
+}
+
+function experience() {
+  return "<h2><span style=\"color:#de935f;\">Experience:</span></h2><table>\
+  <tr>\
+    <td>Software Developer Intern</td>\
+    <td>Triveni IT (June 2024 - Present)</td>\
+  </tr>\
+  <tr>\
+    <td>Data Analyst Intern</td>\
+    <td>NJEDA (Jan 2024 - May 2024)</td>\
+  </tr>\
+  <tr>\
+    <td>Full Stack Developer</td>\
+    <td>Rutgers GRID (Oct 2023 - May 2024)</td>\
+  </tr>\
+  <tr>\
+    <td><a href=\"https://flowingtogether.lambertvillenj.org\" target=\"_blank\">Web Developer</a></td>\
+    <td>Edward J. Bloustein School (Apr 2023 - July 2023)</td>\
+  </tr>\
+  <tr>\
+    <td>Founder & Developer</td>\
+    <td>Tournamentslab.com (Apr 2019 - Feb 2021)</td>\
+  </tr></table>";
+}
+
+
+function projects() {
+  return "<h2><span style=\"color:#b5bd68;\">Projects:</span></h2><table>\
+  <tr><td><a href=\"https://discord.com/oauth2/authorize?client_id=1119329341671747584\" target=\"_blank\">Drag Bot</a></td><td>Facilitating vc drags to 600,000 people.</td></tr>\
+  <tr><td><a href=\"https://github.com/dhawal-arora/Celebelum-AI\" target=\"_blank\">Celebelum</a></td><td>AI voice recognition displayed to 300+ students.</td></tr>\
+  <tr><td><a href=\"https://github.com/dhawal-arora/Patzer\" target=\"_blank\">Patzer</a></td><td>Displaying chess analytics to 200,000+ people.</td></tr>\
+  <tr><td>RUOnTime</td><td>Hackathon winning daily scheduler with bus and class location tracking.</td></tr>\
+  <tr><td>DietRX</td><td>AI tool to suggest what to eat from what dining hall based on health requirements</td></tr>\
+  </table>";
+}
+
+
+function awards() {
+  return "<h2><span style=\"color:#cc6666;\">Awards</span></h2><table>\
+  <tr><td>Rutgers Leadership Fellowship Awardee</td></tr>\
+  <tr><td>HackRU Best Hack Awardee</td></tr>\
+  <tr><td>Employee Recognition Award x2</td></tr>\
+  </table>";
+}
+
+function creativity() {
+  return "<h2><span style=\"color:#f0c674;\">Creative Experience:</span></h2><table>\
+  <tr><td>AV Specialist</td><td>Rutgers Student Affairs (Feb 2023 - Present)</td></tr>\
+  <tr><td>Social Media Manager (2 Million+ Audience)</td><td>All Platforms (Oct 2023 - May 2024)</td></tr>\
+  <tr><td>Cinematography Lead</td><td>Rutgers GSE (Nov 2023 - Jan 2024)</td></tr>\
+  </table>";
+}
+
+function extracurricular() {
+  return "<h2><span style=\"color:#8abeb7;\">Extracurricular:</span></h2><table>\
+  <tr><td>Rutgers Blockchain Hub</td><td>E-board, presenting research and developments to 300+ students</td></tr>\
+  <tr><td><a href=\"https://www.youtube.com/@dhawalchess\" target=\"_blank\">Dhawalplaysd4</a></td><td>YouTube Channel - 300,000+ views</td></tr>\
+  <tr><td><a href=\"https://podcasters.spotify.com/pod/show/thed4podcast\" target=\"_blank\">The D4 Podcast</a></td><td>All Platforms - 2000+ listeners</td></tr>\
+  <tr><td>Rated Chess Player</td><td>FIDE - International Chess Federation</td></tr>\
+  </table>";
+}
+
+function clear() {
+  return "";
+}
+
+function rickroll(){
+  return "<p>We're no strangers to love<br>\
+    You know the rules and so do I<br>\
+    A full commitment's what I'm thinking of<br>\
+    You wouldn't get this from any other guy<br>\
+    <br>\
+    I just wanna tell you how I'm feeling<br>\
+    Gotta make you understand<br>\
+    <br>\
+    Never gonna give you up<br>\
+    Never gonna let you down<br>\
+    Never gonna run around and desert you<br>\
+    Never gonna make you cry<br>\
+    Never gonna say goodbye<br>\
+    Never gonna tell a lie and hurt you<br>\
+    <br>\
+    We've known each other for so long<br>\
+    Your heart's been aching, but<br>\
+    You're too shy to say it<br>\
+    Inside, we both know what's been going on<br>\
+    We know the game and we're gonna play it<br>\
+    <br>\
+    And if you ask me how I'm feeling<br>\
+    Don't tell me you're too blind to see<br>\
+    <br>\
+    Never gonna give you up<br>\
+    Never gonna let you down<br>\
+    Never gonna run around and desert you<br>\
+    Never gonna make you cry<br>\
+    Never gonna say goodbye<br>\
+    Never gonna tell a lie and hurt you<br>\
+    <br>\
+    Never gonna give you up<br>\
+    Never gonna let you down<br>\
+    Never gonna run around and desert you<br>\
+    Never gonna make you cry<br>\
+    Never gonna say goodbye<br>\
+    Never gonna tell a lie and hurt you<br>\
+    <br>\
+    (Ooh, give you up)<br>\
+    (Ooh, give you up)<br>\
+    Never gonna give, never gonna give<br>\
+    (Give you up)<br>\
+    Never gonna give, never gonna give<br>\
+    (Give you up)<br>\
+    <br>\
+    We've known each other for so long<br>\
+    Your heart's been aching, but<br>\
+    You're too shy to say it<br>\
+    Inside, we both know what's been going on<br>\
+    We know the game and we're gonna play it<br>\
+    <br>\
+    I just wanna tell you how I'm feeling<br>\
+    Gotta make you understand<br>\
+    <br>\
+    Never gonna give you up<br>\
+    Never gonna let you down<br>\
+    Never gonna run around and desert you<br>\
+    Never gonna make you cry<br>\
+    Never gonna say goodbye<br>\
+    Never gonna tell a lie and hurt you<br>\
+    <br>\
+    Never gonna give you up<br>\
+    Never gonna let you down<br>\
+    Never gonna run around and desert you<br>\
+    Never gonna make you cry<br>\
+    Never gonna say goodbye<br>\
+    Never gonna tell a lie and hurt you<br>\
+    <br>\
+    Never gonna give you up<br>\
+    Never gonna let you down<br>\
+    Never gonna run around and desert you<br>\
+    Never gonna make you cry<br>\
+    Never gonna say goodbye<br>\
+    Never gonna tell a lie and hurt you</p>";
+}
+
+function commandProcessor(e){
+
+  if(e.keyCode == 13){
+
+    document.getElementById('injected').innerHTML= "";
+
+    var txtInput = document.getElementById('txtBox').value;
+
+    if(txtInput == "help"){
+      document.getElementById('injected').innerHTML=help();
+    }else if (txtInput=="all") {
+      document.getElementById('injected').innerHTML=about() + "\n\n\n" + skills() + "\n\n\n" +experience()+ "\n\n\n" +projects()+ "\n\n\n" +awards()+ "\n\n\n" +creativity()+ "\n\n\n" +extracurricular()+ "\n\n\n" + links() + "\n\n\n" + contact();
+    }else if (txtInput == "about") {
+      document.getElementById('injected').innerHTML=about();
+    }else if (txtInput == "contact") {
+      document.getElementById('injected').innerHTML=contact();
+    }else if (txtInput=="skills") {
+      document.getElementById('injected').innerHTML=skills();
+    }else if (txtInput=="links") {
+      document.getElementById('injected').innerHTML=links();
+    }else if (txtInput=="experience") {
+      document.getElementById('injected').innerHTML=experience();
+    }else if (txtInput=="projects") {
+      document.getElementById('injected').innerHTML=projects();
+    }else if (txtInput=="awards") {
+      document.getElementById('injected').innerHTML=awards();
+    }else if (txtInput=="creativity") {
+      document.getElementById('injected').innerHTML=creativity();
+    }else if (txtInput=="extracurricular") {
+      document.getElementById('injected').innerHTML=extracurricular();
+    }else if (txtInput=="clear") {
+      document.getElementById('injected').innerHTML=clear();
+    }else if (txtInput == "rickroll") {
+      var win = window.open("https://www.paypal.com/paypalme/DhawalA", '_blank');
+      win.focus();
+      document.getElementById('injected').innerHTML=rickroll();
+    }else{
+      document.getElementById('injected').innerHTML = help();
+    }
+
+    //Clear input text box
+    document.getElementById('txtBox').value= "";
+  }
+}
